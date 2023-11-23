a2740506860c4f320e041aac9c6decc0c42ab1bc
niraj <niraj@chegusinfotech>
2023-11-23 11:36:05 +0530

    implemented routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { PostListComponent } from './post/post-list/post-list.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'counter', component: CounterComponent },
+  { path: 'posts', component: PostListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
